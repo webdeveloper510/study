392b3e5bbfc780bd25c9d303b01400e8eae6833d
sapna <developerweb6@gmail.com>
2022-07-19 04:43:50 +0530

    changes

--- src/Router/Router.js
+++ src/Router/Router.js
@@ -5,14 +5,17 @@ import LandingPage from '../Components/landing';
 // import Mcast from '../Components/Mcast';
 // import Omnix from '../Components/Omnix';
 // import Fdp from '../Components/Fdp';
- import Anglia from '../Components/Anglia';
+//import Anglia from '../Components/Anglia';
 //import British from '../Components/British';
+import Vishalscc from '../Components/Vishalscc';
+// import Universityob from '../Components/Universityob';
+// import ManipalU from '../Components/ManipalU';
 
 const Routers = () => {
   return (
       <Router>
         <Routes>
-          <Route exact path="/" element={<Anglia/>} />
+          <Route exact path="/" element={<Vishalscc/>} />
           <Route path="/university/:id" element={<LandingPage />} />
         </Routes>
       </Router>
