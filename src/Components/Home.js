b9a18cfa2a9549571ab009c7b8b028bffc306725
Rekha Thakur <rekhathakur@datepalm.media>
2022-08-03 18:22:54 +0530

    brain animation

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,4 +1,4 @@
-import React,{useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "../App.scss";
 import FlipCard from "./FlipCard";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -33,122 +33,134 @@ import sam from '../images/Seeding Brains UI/Stem1.png'
 import sam1 from '../images/Seeding Brains UI/Stem2.png'
 
 const cards = [
-    {
-      id: "1",
-      variant: "hover",
-      front: "Test University",
-      back: "details Of University"
-    },
-  ];
-  
+  {
+    id: "1",
+    variant: "hover",
+    front: "Test University",
+    back: "details Of University"
+  },
+];
 
-const HomePage = () => {  
-  useEffect(()=>{
-console.log('uni',uniData)
- },[])
- const[uniData,setUniData]=useState(universityData)
-    return (
-        <div>
-         <div className="container">
-           <div className="brain">
-          
-             <div className="image2 position-relative"> <img src={brain2}  height={'250px'} alt="" className="main"/> 
-             <div className="brainimag7">
-               <div className="collage">
-               <img className="" src={image} height={'150px'} alt=""/>
-               </div>
-               <div className="collage1">
-               <img  src={image16} height={'80px'} alt=""/>
-               </div>
-               <div className="collage2">
-               <img className="" src={image2} height={'150px'} alt=""/>
-               </div>
-                
-               </div></div>
-             <div className="image5 position-relative"> <img src={brain5} height={'250px'} alt="" className="main"/> 
-             <div className="brainimag6">
-               <div className="collage3">
-                   <img className="" src={image10} height={'150px'} alt=""/>
-               </div>
-               <div className="collage4">
-                  <img className="" src={image17} height={'80px'} alt=""/>
-               </div>
-               <div className="collage5">
-                 <img className="" src={image12} height={'150px'} alt=""/>
-               </div>
-                
-               </div></div>
-             <div className="image3 position-relative"> <img src={brain3} height={'200px'} alt="" className="main"/> 
-             <div className="brainimag5">
-               <div className="collage6">
-                    <img className="" src={image7} height={'150px'} alt=""/>
-               </div>
-               <div className="collage7">
-                    <img className="" src={image8} height={'150px'} alt=""/>
-               </div>
-               <div className="collage8">
-                    <img className="p-3" src={image9} height={'120px'} alt=""/>
-               </div>
-                 
-               </div></div>
-             <div className="image4 position-relative"> <img src={brain4} height={'180px'} alt="" className="main"/> 
-             <div className="brainimag4">
-                 <div className="collage9">
-                   <img className="" src={image3} height={'150px'} alt=""/>
-                 </div>
-                 <div className="collage10">
-                     <img className="" src={image5} height={'150px'} alt=""/>
-                 </div>
-                 <div className="collage11">
-                    <img className="" src={image6} height={'150px'} alt=""/>
-                 </div>
-                
-               </div></div>
-             
-             <div className="image6 position-relative"> <img src={brain6} height={'150px'} alt="" className="main"/> 
-             <div className="brainimag3">
-               <div className="collage12">
-               <img className="" src={image13} height={'150px'} alt=""/>
-               </div>
-               <div className="collage13">
-               <img className="" src={image14} height={'150px'} alt=""/>
-               </div>
-               <div className="collage14">
-               <img className="" src={image15} height={'150px'} alt=""/>
-               </div>
-               </div></div>
-             <div className="image7 position-relative"> <img src={brain7} height={'125px'} alt="" className="main"/> 
-             <div className="brainimag2">
-               <div className="collage15">
-               <img className="" src={image1} height={'150px'}  alt=""/>
-               </div>
-               <div className="collage16">
-               <img className="" src={image11} height={'150px'} alt=""/>
-               </div>
-               </div>
-             </div>
-             <div className="image1 position-relative"> <img src={brain1} height={'340px'} alt="" className="main"/>
-               <div className="brainimag1">
-                  <img className="collage24" src={sam} height={'150px'} alt=""/>
-                  <img className="collage25" src={sam1} height={'150px'} alt=""/>
-               </div>
+
+const HomePage = () => {
+  useEffect(() => {
+    console.log('uni', uniData)
+  }, [])
+  const [uniData, setUniData] = useState(universityData)
+  return (
+    <div>
+      <div className="container">
+        <div className="brain">
+
+          <div className="image2 position-relative"> <img src={brain2} height={'250px'} alt="" className="main" />
+            <div className="brainimag7">
+              <div className="collage">
+                <img className="" src={image} height={'150px'} alt="" />
+              </div>
+              <div className="collage1">
+                <img src={image16} height={'80px'} alt="" />
+              </div>
+              <div className="collage2">
+                <img className="" src={image2} height={'150px'} alt="" />
               </div>
 
-           </div>
+            </div></div>
+          <div className="image5 position-relative"> <img src={brain5} height={'250px'} alt="" className="main" />
+            <div className="brainimag6">
+              <div className="collage3">
+                <img className="" src={image10} height={'150px'} alt="" />
+              </div>
+              <div className="collage4">
+                <img className="" src={image17} height={'80px'} alt="" />
+              </div>
+              <div className="collage5">
+                <img className="" src={image12} height={'150px'} alt="" />
+              </div>
+
+            </div></div>
+          <div className=" position-relative"> 
+          <span className="image3">
+          <img src={brain3} height={'200px'} alt="" className="main" />
+            <div className="brainimag5">
+              <div className="collage6">
+                <img className="" src={image7} height={'150px'} alt="" />
+              </div>
+              <div className="collage7">
+                <img className="" src={image8} height={'150px'} alt="" />
+              </div>
+              <div className="collage8">
+                <img className="p-3" src={image9} height={'120px'} alt="" />
+              </div>
+
+            </div>
+            </span>
+            </div>
+          <div className=" position-relative">
+            <span className="image4">
+              <img src={brain4} height={'180px'} alt="" className="main" />
+
+              <div className="brainimag4">
+                <div className="collage9">
+                  <img className="" src={image3} height={'150px'} alt="" />
+                </div>
+                <div className="collage10">
+                  <img className="" src={image5} height={'150px'} alt="" />
+                </div>
+                <div className="collage11">
+                  <img className="" src={image6} height={'150px'} alt="" />
+                </div>
+
+              </div>
+            </span>
+          </div>
+
+          <div className="image6 position-relative"> <img src={brain6} height={'150px'} alt="" className="main" />
+            <div className="brainimag3">
+              <div className="collage12">
+                <img className="" src={image13} height={'150px'} alt="" />
+              </div>
+              <div className="collage13">
+                <img className="" src={image14} height={'150px'} alt="" />
+              </div>
+              <div className="collage14">
+                <img className="" src={image15} height={'150px'} alt="" />
+              </div>
+            </div></div>
+          <div className="image7 position-relative"> <img src={brain7} height={'125px'} alt="" className="main" />
+            <div className="brainimag2">
+              <div className="collage15">
+                <img className="" src={image1} height={'150px'} alt="" />
+              </div>
+              <div className="collage16">
+                <img className="" src={image11} height={'150px'} alt="" />
+              </div>
+            </div>
+          </div>
+          <div className=" position-relative">
+            <span className="image1">
+             <img src={brain1} height={'340px'} alt="" className="main" />
+            <div className="brainimag1">
+              <img className="collage24" src={sam} height={'150px'} alt="" />
+              <img className="collage25" src={sam1} height={'150px'} alt="" />
+            </div>
+            </span>
+          </div>
+
+        </div>
 
 
 
-      <div className="row ">
-       
+        <div className="row ">
+
           {uniData.map((card) => (
-             <div className="col-md-3">
-            <FlipCard key={card.id} card={card} />
+            <div className="col-md-3">
+              <FlipCard key={card.id} card={card} />
             </div>
           ))}
-        
+
+        </div>
       </div>
     </div>
-            </div>
-    )
+  )
 }
 export default HomePage;
