554fc549ae6c610b647b26893c1567e78f952e41
Rekha Thakur <rekhathakur@datepalm.media>
2022-08-11 02:39:51 +0530

    changes

--- src/Components/Mcast.js
+++ src/Components/Mcast.js
@@ -3,172 +3,192 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Logo from '../images/MCAST1.jpg'
 import LogoHome from '../images/MCAST.png'
 import telephone from '../images/MCAST2.jpg'
 import Breadcrumb from 'react-bootstrap/Breadcrumb';
+import logo from '../images/Seeding Brains UI/logo.png'
+import { Button } from 'react-bootstrap';
 
 function Mcast() {
   return (
     <>
-     <Navbar expand="lg" className='navbars'>
-      <Container className='p-0'>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mx-auto">
-            <Nav.Link href="#home">HOME </Nav.Link>
-            <Nav.Link href="#link">ABOUT US</Nav.Link>
-            <Nav.Link href="#link">SERVICE </Nav.Link>
-            <Nav.Link href="#link">COURSES </Nav.Link>
-            <Nav.Link href="#link">CONTACT US</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-    
+      <div className='header'>
+        <Navbar bg="light" expand="lg">
+          <Container>
+            <Navbar.Brand href="#home"><img src={logo} className="img-fluid" /></Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="#home">HOME</Nav.Link>
+                <Nav.Link href="#link">ABOUT US</Nav.Link>
+                <Nav.Link href="#link">SERVICE</Nav.Link>
+                <Nav.Link href="#link">COURSES</Nav.Link>
+                <Nav.Link href="#link">CONTACT US</Nav.Link>
+                <Button variant="danger">Search</Button>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </div>
+      <div className='Credentials'>
 
-    <div className='Credentials'>
-     
         <Container>
-            <div className='text-right'>
-            <img src={LogoHome} width="420px" height="120px" alt="" />
+          <div className="">
+            <Breadcrumb>
+              <Breadcrumb.Item href="#">Home</Breadcrumb.Item>
+              <Breadcrumb.Item active>MCAST</Breadcrumb.Item>
+            </Breadcrumb>
+            <h3 className='my-5'>GET A EUROPEAN DEGREE AND WORK OPPORTUNITIES IN THE EUROPEAN REGION </h3>
+            <div className='home_logo my-5'>
+              <img src={LogoHome} width="420px" height="120px" alt="" />
             </div>
-        <Breadcrumb>
-            <Breadcrumb.Item href="#">Home</Breadcrumb.Item>
-            <Breadcrumb.Item active>MCAST</Breadcrumb.Item>
-        </Breadcrumb>
-       
-           <h3>GET A EUROPEAN DEGREE AND WORK <br/> OPPORTUNITIES IN THE EUROPEAN REGION </h3>
+          </div>
+          <h2>Overview</h2>
+          <p>MCAST provides an excellent chance for students looking for an International
+            Qualification. Our Programs are designed by the Industry professionals, has a
+            history of turning out graduates with the top talents that organizations demand
+            and get chance to be a part of the European Region.
+          </p>
+          <Button variant="danger">Apply Now</Button>
 
-           <ul>
-               <li>Apprenticeship</li>
-               <li>Diploma in furniture</li>
-               <li> ME</li>
-               <li>EE</li>
-           </ul>
-           <div className='my-4'>
-           <img src={Logo} width="100%" height="600px" alt="" />
-           </div>
-
-        
-            <p className='mt-5'> <b>Certificate : </b> Apprenticeship, Diploma in furniture, ME, EE,</p>
-            <p><b>Instructors:</b> <a href="https://intranet.mcast.edu.mt/">https://intranet.mcast.edu.mt/</a></p>
-            <p className='pb-5'><b>Faculty :</b></p>
         </Container>
-    </div>
-    <div className='Syllabus'>
+      </div>
+      <div className='offered'>
         <Container>
-        <h3>Program Structure :</h3>
-           <ul>
-               <li>Level 4+ 5 + 6  </li>
-                <li>Level 4+5 and Top-Up</li>
-           </ul>
-           <p><b>Level 4 (1st Year of Graduation)</b><br/> <b>Credits: 120</b> <br/> <b>Guided Learning Hours: 480</b> <br/><b>Core Units:</b> </p>
-           
-           <ul>
-             <li>Business & Business Environment</li>
-             <li>Marketing Process & Planning</li>
-             <li>Human Resource Management</li>
-             <li>Leadership & Management</li>
-             <li>Accounting Principles</li>
-             <li>Managing a Successful Business Project</li>
-           </ul>
           <Row>
-            <Col md={6}>
-            <p><b>Optional Unit:</b></p>
-           <ul>
-             <li>Innovation and Commercialisation</li>
-           </ul>
-           <p><b>After Completion of the 1st Year the student Progresses to Level 5.</b></p>
-           <p><b>Level 5: (2nd Year of Graduation)</b><br/> <b>Credits: 120</b> <br/> <b>Guided Learning Hours: 480</b> <br/><b>Core Units:</b> </p>
-             <ul>
-               <li>Research Project (Pearson Set)</li>
-               <li>Organizational Behaviour</li>
-             </ul>
-             <p><b>Optional Unit:</b></p>
-             <ul>
-               <li>Understanding and Leading Change</li>
-               <li>Global Business Environment</li>
-               <li>Principles of Operations Management</li>
-               <li>Business Strategy</li>
-               <li>International Marketing</li>
-             </ul>
-           <p><b>Level 6: (3rd Year of Graduation)</b><br/> <b>Credits: 120</b> <br/> <b>Guided Learning Hours: </b></p>
-           <p><b>Core Units:</b></p>
-           <ul>
-             <li>Financial Principles and Techniques for Non-specialists</li>
-             <li>Strategic Management</li>
-             <li>Innovation and Creativity in Business</li>
-             <li>Supply Chain Management</li>
-             <li>Management and Leadership</li>
-             <li>Entrepreneurship in Business</li>
-             <li>Change Management</li>
-             <li>Contemporary Issues in Business</li>
-           </ul>
-            <p><b>Top Up:</b><br/> <b>Credits: 60/90/120</b></p>
-            <p><b>University TieUps:</b></p>
-            <ul>
-              <li>Anglia Rukin University</li>
-              <li>Bolton University</li>
-              <li>University of Wales Trinity Saint David</li>
-              <li>University of Gloucestershire</li>
-              <li>Derby University & Many More.</li>
-            </ul>
-            <p><b>Mode of Learning:</b>  Online / On Campus</p>
+            <Col md="6">
+              <div className=''>
+                <h2 className='mb-5'>Qualifications Offered</h2>
+                <ul>
+                  <li>MBA -
+                    <ol>
+                      <li>1) Business</li>
+                      <li>2) Small Business</li>
+                    </ol>
+                  </li>
+                  <li>
+                    MSc ( Information Technology & Systems )
+                  </li>
+                  <li>
+                    Combination Program (PGDBM - MBA)
+                  </li>
+                </ul>
+              </div>
             </Col>
-            <Col md={6}>
-              <img src={telephone} className="img-fluid" alt="" />
+            <Col md="6">
+              <div className='programs'>
+                <p>Our MBA program seeks to build confident, forward- thinking executives who can mix strategic
+                  thinking with excellent operational focus in prder to meet the evidence-based requirement for qualified managers.
+                </p>
+              </div>
             </Col>
           </Row>
-          
-         
+          <p>Mode of Learning : Online / On Campus</p>
         </Container>
-    </div>
-
-    <div className='Benefits'>
+      </div>
+      <div className='Syllabus'>
         <Container>
-          <p><b>Level 7 - Postgraduate Diploma in Business and Administration</b></p>
-          <p><b>Level 7 - Learning Outcomes.</b></p>
-          <p><b>Postgraduate Diploma in Business and Administration</b></p>
-          <ol>
-            <li>Demonstrate professionalism, self-awareness, leadership, and effective communication in a team based environment.</li>
-            <li>Possess the skills required to integrate concepts from various disciplines to identify and develop business strategies.</li>
-            <li>Demonstrate entrepreneurial, creative and innovative skills in managing and running an organisation.</li>
-            <li>Apply knowledge and skills to solve business problems.</li>
-            <li>Analyze and apply accounting information to facilitate strategic decision making.</li>
-            <li>Employ financial decision models to select appropriate projects for a business enterprise.</li>
-          </ol>
+          <h3>Home > MCAST > MBA</h3>
+          <h4>Masters of Business Administration (MBA) for the SMALL BUSINESS</h4>
+          <h2>Key Highlights</h2>
+          <Row>
+            <Col md="4">
+              <div className='key'>
+                <ol>
+                  <li>Flexible Intakes</li>
+                  <li>Flexible Mode of Study</li>
+                  <li>Fast Track</li>
+                  <li>Assignment Based Assessment</li>
+                  <li> Doubt Sessions </li>
+                </ol>
+              </div>
+            </Col>
+            <Col md="4">
+              <div className='key'>
+                <ol>
+                  <li>Real Time Case Studies</li>
+                  <li>Industry Expert Live Session</li>
+                  <li>Career Assistance</li>
+                  <li>Duration - 2 Years</li>
+                  <li>Credits - 90 ECTS</li>
+                </ol>
+              </div>
 
-          <p><b>Core Units:</b></p>
-          <ol>
-            <li>Applied Research & Development</li>
-            <li>Business Analytics</li>
-            <li>Dynamic Strategic Management</li>
-            <li>Development Economics</li>
-            <li>Accounting for Managers</li>
-            <li>Contemporary Entrepreneurship Management</li>
-            <li>Strategic International marketing</li>
-            <li>Project Management</li>
-            <li>Financial Analysis</li>
-            <li>Organisational Behaviour</li>
-          </ol>
-          <p><b style={{color:'red'}}>Entry Requirement:</b></p>
-          <ul>
-            <li>Relevant & Related Level 3 qualification in the Prefered Area of Specialization</li>
-            <li>GCE Advanced Level profile that demonstrates strong performance in a relevant subject or adequate performance in more than one GCE subject. </li>
-            <li>An Access to Higher Education Diploma from an approved further education institution</li>
-            <li>Relevant work experience</li>
-            <li>An international equivalent to the above qualifications.</li>
-          </ul>
-          <p>Our Recognition of Prior Learning policy means that students’ previous learning and experience can be taken into account and they may be awarded certain qualifications or units of a qualification based on that learning or experience.</p>
-            <h3 style={{color:'red'}}>Benefits :</h3>
-            <p>Post Study Work Visa / Paid Internships </p>
-            <p> <b style={{color:'red'}}>Mode of Study: </b> Online </p>
-            <p><b style={{color:'red'}}>Fees:</b></p>
+            </Col>
+            <Col md="4">
+              <div className='entry'>
+                <h3>
+                  Entry Requirements
+                </h3>
+                <p>First degree in business; management or a technological programme.</p>
+                <p>Candidates  must also have 2 years work experience.</p>
+              </div>
+            </Col>
+          </Row>
+          <div className=''>
+            <h3>Course Overview</h3>
+            <p>The MBA for the Small Business programme focuses on the theoretical knowledge, application-oriented experiences, and identification of innovation possibilities and growth potentials of the small business or strategic business units. Learners will be able to understand the fundamental concepts of business management, business development, as well as operations and project management within a small business. MCAST works very closely with small businesses to design and continuously develop this MBA programme that comprises 10 modules and a Master’s Dissertation.
+            </p>
+          </div>
+          <Row>
+            <Col md="9">
+              <h3>Learning Outcomes</h3>
+              <h5>Upon successful completion of this Masters Programme the learners will be able to: </h5>
+              <p> - Understand and satisfy the business needs and requirements of micro and small businesses within local industry.
+              </p>
+              <p> - Identify innovation possibilities and growth potentials within choice companies, and act to address these possibilities.</p>
+              <p> - Develop critical thinking abilities and foster analytical skills to address today’s toughest business challenges.</p>
+              <p> - Act as a business leader who is capable of leading, motivating and operating within a small business venture</p>
+            </Col>
+            <Col md="3">
+               <img src='' className='img-fluid'/>
+            </Col>
+          </Row>
+          <div className='Instructors'>
+            <h4>Instructors: https://intranet.mcast.edu.mt/</h4>
+          </div>
+
+              <h3>Program Structure:</h3>
+              <h4> - Academic Year 1 (Semesters 1 & 2: 30 ECTS) – Exit Option: Post Graduate Certificate</h4>
+             <Row>
+              <Col md={6}>
+              <ul>
+                <li>Applied Research and Development</li>
+                <li>Business Analytics</li>
+              </ul>
+              </Col>
+              <Col md={6}>
+              <ul>
+                <li>Strategy and Small Business Growth</li>
+                <li>Lean Start-up</li>
+                <li>Accounting for Managers</li>
+              </ul>
+              </Col>
+             </Row>
+             
+              <h4> - Academic Year 2 (Semesters 3 & 4: 30 ECTS) – Exit Option: Post Graduate Diploma</h4>
+              <ol>
+                <li>Entrepreneurship & Innovation Management</li>
+                <li>Development Economics</li>
+                <li>Small Business Marketing</li>
+              </ol>
+              <ol>
+                <li> Elective Advanced Professional Studies - (Any 2 Modules)</li>
+                <li>Basics of Project Management</li>
+                <li>Financial Analysis</li>
+                <li>Lean Leadership</li>
+              </ol>
+             <h4> - Academic Year 3 (Semester 5: 30 ECTS) – Final Exit: Masters</h4>
+             <ol>
+               <li>Dissertation</li>
+             </ol>
+             <h4>Fee Structure: </h4>
         </Container>
-    </div>
+      </div>
+
     </>
   )
 }
