6a03129c411fe7ab410777c0492d295261b80168
Rekha Thakur <rekhathakur@datepalm.media>
2022-10-07 21:49:58 +0530

    changes

--- src/Components/desk.js
+++ src/Components/desk.js
@@ -20,8 +20,8 @@ function desk() {
               <Navbar.Toggle aria-controls="basic-navbar-nav" />
               <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ms-auto">
-                  <Nav.Link  href="/business/BusinessPage">Home</Nav.Link>
-                  <Nav.Link className='active' href="/business/Desk">Destination</Nav.Link>
+                  <Nav.Link  href="/BusinessPage">Home</Nav.Link>
+                  <Nav.Link className='active' href="/Desk">Destination</Nav.Link>
                   <Nav.Link href="#link">About</Nav.Link>
 
                 </Nav>
